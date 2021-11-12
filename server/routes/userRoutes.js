90aea5ed53cb71b2e4e4b44dda6666f64298ddc1
AipBorges <mpbds@uic.es>
2021-11-12 21:02:49 +0100

    user model done

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+/* const bcrypt = require("bcrypt"); */
+const { validate } = require("express-validation");
+/* const { loginUser } = require("../controllers/usersControllers");
+const { loginSchema } = require("../schemas/userSchema"); */
+
+const router = express.Router();
+
+/* router.post("/login", validate(loginSchema) loginUser); */
+
+module.exports = router;
